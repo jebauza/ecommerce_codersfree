9d38baefb03de49be70a6fb5373f9270126e64e6
Jorge Ernesto Bauza <jbauza.becerra@servinform.es>
2022-04-23 16:52:53 +0200

    ready sanctum

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
 
 module.exports = {
     content: [
@@ -13,7 +14,31 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
-        },
+            // colors: {
+            //     transparent: 'transparent',
+            //     current: 'currentColor',
+            //     //amber: colors.amber,
+            //     black: colors.black,
+            //     blue: colors.blue,
+            //     cyan: colors.cyan,
+            //     emerald: colors.emerald,
+            //     fuchsia: colors.fuchsia,
+            //     gray: colors.gray,
+            //     green: colors.green,
+            //     indigo: colors.indigo,
+            //     lime: colors.lime,
+            //     orange: colors.orange,
+            //     pink: colors.pink,
+            //     purple: colors.purple,
+            //     red: colors.red,
+            //     rose: colors.rose,
+            //     sky: colors.sky, //warn - As of Tailwind CSS v2.2, `lightBlue` has been renamed to `sky`.
+            //     teal: colors.teal,
+            //     violet: colors.violet,
+            //     yellow: colors.amber,
+            //     white: colors.white,
+            // },
+        }
     },
 
     plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
