30c8c4731fc7e1f0ff895bd338930e6110c14d6e
Jorge Ernesto Bauza <jebauza1989@gmail.com>
2023-01-12 22:59:16 +0100

    responsive view category show

--- app/Http/Livewire/CategoryFilter.php
+++ app/Http/Livewire/CategoryFilter.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -9,14 +10,31 @@ class CategoryFilter extends Component
 {
     use WithPagination;
 
-    public $category;
+    public $category, $searchSubcategoryId, $searchBrandId;
+    public $view = 'grid';
 
     public function render()
     {
         $products = $this->category->products()
                                     ->published()
+                                    ->when($this->searchSubcategoryId, function (Builder $query) {
+                                        return $query->where('products.subcategory_id', $this->searchSubcategoryId);
+                                    })
+                                    ->when($this->searchBrandId, function (Builder $query) {
+                                        return $query->where('products.brand_id', $this->searchBrandId);
+                                    })
                                     ->paginate(20);
 
         return view('livewire.category-filter', compact('products'));
+    }
+
+    /**
+     * Method clear
+     *
+     * @return void
+     */
+    public function clear()
+    {
+        $this->reset(['searchSubcategoryId', 'searchBrandId']);
     }
 }
--- resources/views/livewire/category-filter.blade.php
+++ resources/views/livewire/category-filter.blade.php
@@ -1,48 +1,116 @@
 <div>
-    <div class="bg-white rounded-lg shadow-lg mb-4">
+    <div class="bg-white rounded-lg shadow-lg mb-6">
         <div class="px-6 py-2 flex justify-between items-center">
             <h1 class="font-semibold text-gray-700 uppercase">{{ $category->name }}</h1>
 
             <div class="grid grid-cols-2 border-gray-200 divide-x divide-gray-200 text-gray-500">
-                <i class="fas fa-border-all p-3 cursor-pointer"></i>
-                <i class="fas fa-th-list p-3"></i>
+                <i wire:click="$set('view', 'grid')" class="fas fa-border-all p-3 cursor-pointer {{ $view == 'grid' ? 'text-orange-500' : '' }}"></i>
+                <i wire:click="$set('view', 'list')" class="fas fa-th-list p-3 cursor-pointer {{ $view == 'list' ? 'text-orange-500' : '' }}"></i>
             </div>
         </div>
     </div>
 
-    <div class="grid grid-cols-5">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
         <aside>
             <h2 class="font-semibold text-center mb-2">Subcategorías</h2>
-
-            <ul>
+            <ul class="divide-y divide-gray-200">
                 @foreach ($category->subcategories as $subcategory)
-                    <li class="my-2 text-sm">
-                        <a href="" class="cursor-pointer hover:text-orange-500 capitalize">{{ $subcategory->name }}</a>
+                    <li class="py-2 text-sm">
+                        <a wire:click="$set('searchSubcategoryId', '{{ $subcategory->id }}')"
+                            class="cursor-pointer hover:text-orange-500 capitalize {{ $searchSubcategoryId == $subcategory->id ? 'text-orange-500 font-semibold' : '' }}">{{ $subcategory->name }}</a>
                     </li>
                 @endforeach
             </ul>
+
+            <h2 class="font-semibold text-center mt-4 mb-2">Marcas</h2>
+            <ul class="divide-y divide-gray-200">
+                @foreach ($category->brands as $brand)
+                    <li class="py-2 text-sm">
+                        <a wire:click="$set('searchBrandId', '{{ $brand->id }}')"
+                            class="cursor-pointer hover:text-orange-500 capitalize {{ $searchBrandId == $brand->id ? 'text-orange-500 font-semibold' : '' }}">{{ $brand->name }}</a>
+                    </li>
+                @endforeach
+            </ul>
+
+            <x-jet-button wire:click="clear" class="mt-4">
+                Eliminar filtros
+            </x-jet-button>
         </aside>
 
-        <div class="col-span-4">
-            <ul class="grid grid-cols-4 gap-6">
-                @foreach ($products as $product)
-                    <li class="bg-white rounded-lg shadow">
-                        <article>
-                            <figure>
-                                <img class="object-cover object-center w-full h-48" src="{{ Storage::url($product->images->first()->url) }}" alt="">
-                            </figure>
+        <div class="md:col-span-2 lg:col-span-4">
+            @if ($view == 'grid')
+                <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                    @foreach ($products as $product)
+                        <li class="bg-white rounded-lg shadow">
+                            <article>
+                                <figure>
+                                    <img class="object-cover object-center w-full h-48" src="{{ Storage::url($product->images->first()->url) }}" alt="">
+                                </figure>
 
-                            <div class="px-6 py-4">
-                                <h1 class="text-lg font-semibold">
-                                    <a href="">{{ Str::limit($product->name, 20) }}</a>
-                                </h1>
+                                <div class="px-6 py-4">
+                                    <h1 class="text-lg font-semibold">
+                                        <a href="">{{ Str::limit($product->name, 20) }}</a>
+                                    </h1>
 
-                                <p class="font-bold text-gray-700">US$ {{ $product->price }}</p>
-                            </div>
-                        </article>
-                    </li>
-                @endforeach
-            </ul>
+                                    <p class="font-bold text-gray-700">US$ {{ $product->price }}</p>
+                                </div>
+                            </article>
+                        </li>
+                    @endforeach
+                </ul>
+            @else
+                <ul>
+                    @foreach ($products as $product)
+                        <li class="bg-white rounded-lg shadow mb-4">
+                            <article class="flex">
+                                <figure>
+                                    <img class="h-48 w-56 object-cover object-center" src="{{ Storage::url($product->images->first()->url) }}" alt="">
+                                </figure>
+
+                                <div class="flex-1 py-4 px-6 flex flex-col">
+                                    <div class="flex justify-between">
+                                        <div>
+                                            <h1 class="text-lg font-semibold text-gray-700">{{ $product->name }}</h1>
+                                            <p class="font-bold text-gray-700">US {{ $product->price }}</p>
+                                        </div>
+
+                                        <div class="flex items-center">
+                                            <ul class="flex text-sm">
+                                                <li>
+                                                    <i class="fas fa-star text-yellow-400 mr-1"></i>
+                                                </li>
+                                                <li>
+                                                    <i class="fas fa-star text-yellow-400 mr-1"></i>
+                                                </li>
+                                                <li>
+                                                    <i class="fas fa-star text-yellow-400 mr-1"></i>
+                                                </li>
+                                                <li>
+                                                    <i class="fas fa-star text-yellow-400 mr-1"></i>
+                                                </li>
+                                                <li>
+                                                    <i class="fas fa-star text-yellow-400 mr-1"></i>
+                                                </li>
+                                                <li>
+                                                    <i class="fas fa-star text-yellow-400 mr-1"></i>
+                                                </li>
+                                            </ul>
+
+                                            <span class="text-gray-700 text-sm">(24)</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="mt-auto mb-6">
+                                        <x-jet-danger-button>
+                                            Más información
+                                        </x-jet-danger-button>
+                                    </div>
+                                </div>
+                            </article>
+                        </li>
+                    @endforeach
+                </ul>
+            @endif
 
             <div class="mt-4">
                 {{ $products->links() }}
