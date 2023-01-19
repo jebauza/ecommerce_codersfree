e8b4b8e368eb2d1395fff8b57c90dfecb0f6ee08
Jorge Ernesto Bauza <j.bauza@mail.zeus.vision>
2023-01-19 17:00:44 +0100

    error favicon.ico"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
+        <link rel="shortcut icon" href="#">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
